b53ba2b9faa5a0ae11ee76da3629434470487395
“wuuud” <syrdiy@gmail.com>
2022-11-04 20:33:08 +0900

    Initial Commit

---
+++ VolunteerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VolunteerApp
+//
+//  Created by H M on 2022/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
